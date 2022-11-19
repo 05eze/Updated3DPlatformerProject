127d7c677e65c9a3bbb944c8a909db039cffda99
05eze <esintim2022@elam.co.uk>
2022-11-19 13:40:51 +0000

    Moving Platform code (Player slides off)

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -8,14 +8,35 @@ public class MovingPlatform : MonoBehaviour
     public Transform myStartPoint;
     public Transform myEndPoint;
 
+    public float speed;
+
+    bool isReversing = false;
 
     void Start()
     {
-        
+        myPlatform.position = myStartPoint.position;
     }
 
     void Update()
     {
-        
+        if (isReversing == false)
+        {
+            myPlatform.position = Vector3.MoveTowards(myPlatform.position, myEndPoint.position, speed);
+
+            if (myPlatform.position == myEndPoint.position)
+            {
+                isReversing = true;
+            }
+        }
+        else
+        {
+            myPlatform.position = Vector3.MoveTowards(myPlatform.position, myStartPoint.position, speed);
+
+            if (myPlatform.position == myStartPoint.position)
+            {
+                isReversing = false;
+            }
+        }
+
     }
 }
