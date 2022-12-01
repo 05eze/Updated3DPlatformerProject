60ad424982aeac8ebc1aa922adbb49575f16605a
05eze <esintim2022@elam.co.uk>
2022-12-01 15:33:27 +0000

    Animations Implemented
    Some animations do not work correctly

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float maxSpeed;
+    public float normalSpeed = 0.2f;
+    public float sprintSpeed = 0.4f;
+
+    float rotation = 0.0f;
+    float camRotation = 0.0f;
+    public float rotationSpeed = 2.0f;
+    public float camRotationSpeed = -1.5f;
+    public float jumpForce = 2.0f;
+    GameObject cam;
+    Rigidbody myRigidbody;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+
+    public float maxSprint = 0.5f;
+    float sprintTimer;
+
+    Animator myAnim;
+
+    void Start()
+    {
+        myAnim = GetComponentInChildren<Animator>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+        sprintTimer = maxSpeed;
+
+        cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    
+    void Update()
+    {
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+        myAnim.SetBool("isOnGround", isOnGround);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myAnim.SetTrigger("jumped");
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed = sprintSpeed;
+            sprintTimer = sprintTimer - Time.deltaTime;
+        } else
+        {
+            maxSpeed = normalSpeed;
+            if (Input.GetKey(KeyCode.LeftShift) == false) {
+                sprintTimer = sprintTimer + Time.deltaTime;
+            }
+        }
+
+        sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
+
+        //transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+
+        Vector3 newVelocity = (transform.forward * Input.GetAxis("Vertical") * maxSpeed) + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+
+        myAnim.SetFloat("speed", newVelocity.magnitude);
+
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
+
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+
+        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        camRotation = Mathf.Clamp(camRotation, -40.0f, 40.0f);
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
+    }
+}
