0824bd4644b5814ef568bceb90294f79f2640002
05eze <esintim2022@elam.co.uk>
2022-12-05 14:53:52 +0000

    Enemy AI (Incomplete)

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -43,13 +43,37 @@ public class EnemyAi : MonoBehaviour
 
     private void Patroling()
     {
-        if (!walkPointSet) SearchWalkPoint();
+        if(!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
     }
 
     private void SearchWalkPoint()
     {
         //Calculate random point in range
-        float randomZ
-            //2:15 https://www.youtube.com/watch?v=UjkSFoLxesw
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //3:22 https://www.youtube.com/watch?v=UjkSFoLxesw&list=WL&index=1
     }
 }
