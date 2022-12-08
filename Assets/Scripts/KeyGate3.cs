c44ed1772df966bd26f86b4b110ef7c7c1becbb2
05eze <esintim2022@elam.co.uk>
2022-12-08 14:28:02 +0000

    Working doors, hints - NEED TO FIX PLATFORM SPEED

---
+++ Assets/Scripts/KeyGate3.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyGate3 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Player" && GameVariables.keyCount > 0 + 4)
+        {
+            GameVariables.keyCount--;
+            Destroy(gameObject);
+        }
+    }
+}
